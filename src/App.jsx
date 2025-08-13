132d75c66672f0c8a11ddb87c6e85a4dd444cbe4
abdulaziz <abdulazizabdu707@gmail.com>
2025-08-13 09:14:07 +0300

    aa

--- src/App.jsx
+++ src/App.jsx
@@ -60,3 +60,20 @@ root.render(
 </StrictMode>
 )
 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Keshf from './components/Keshf';
+import Home from './components/Home';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/keshf" element={<Keshf />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
+
---
+++ src/keshf.jsx
@@ -0,0 +1,11 @@
+// src/components/Keshf.jsx
+import React from 'react';
+
+export default function Keshf() {
+  return (
+    <div>
+      <h1>Take your عقيدتك</h1>
+      {/* copy content from keshf.html here */}
+    </div>
+  );
+}
